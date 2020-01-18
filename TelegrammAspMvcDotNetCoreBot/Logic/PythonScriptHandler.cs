a25e4e8379ec5a599d25ae19f3076084f459cbc2
Evleksey <alzer35@gmail.com>
2020-01-18 20:51:43 +0300

    Python scrips introduced

---
+++ TelegrammAspMvcDotNetCoreBot/Logic/PythonScriptHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IronPython.Hosting;
+using IronPython.Compiler;
+
+namespace TelegrammAspMvcDotNetCoreBot.Logic
+{
+    public static class PythonScriptHandler
+    {
+        public static object PythonRunner (Dictionary<string, object> dictionary, string scriptPath)
+        {
+            var engine = Python.CreateEngine(); // Extract Python language engine 
+            var scope = engine.CreateScope(); // Introduce Python namespace 
+            
+            scope.SetVariable("params", dictionary); 
+
+            var source = engine.CreateScriptSourceFromFile(scriptPath); // Load the script
+            object result = source.Execute(scope);
+            return result;
+        }
+    }    
+}
